4f5ec50b6d5d3adbdebda332cabc55cc7f13303e
zmzimpl <zmzimpl@gmail.com>
2024-01-21 20:52:24 +0800

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,2 @@
+export const LOADER_FRAMES = ["-", "\\", "|", "/"];
+
---
+++ index.js
@@ -0,0 +1,28 @@
+import { readFileSync, promises, existsSync, writeFileSync } from "fs";
+import {
+  getDir,
+  logIntro,
+  sleep,
+  logWork,
+} from "./utils/index.js";
+import consoleStamp from "console-stamp";
+import axios from "axios";
+import chalk from "chalk";
+
+const config = JSON.parse(readFileSync(getDir("config.json"), "utf8"));
+
+const main = async () => {
+
+
+  const execute = async () => {
+
+  };
+
+  execute();
+};
+
+logIntro();
+consoleStamp(console, {
+  format: ":date(yyyy/mm/dd HH:MM:ss)",
+});
+main();

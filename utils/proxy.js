f241d8519f925177b0aa05b89975e0fba4b9055c
zmzimpl <zmzimpl@gmail.com>
2024-02-02 15:31:51 +0800

    修改 proxy 方法

--- utils/proxy.js
+++ utils/proxy.js
@@ -1,42 +1,14 @@
-export const agentClient = (proxy) => {
-    if (!proxy) return;
-    const proxyConfig = proxy.split('://')[0].toLowerCase();
-    let proxyType, host, port, username, password, agent, agentType;
-    if (proxyConfig.length > 1) {
-        proxyType = proxyConfig[0];
-        [host, port, username, password] = proxyConfig[1].split(':')
-    } else {
-        [host, port, username, password] = proxyConfig[0].split(':')
-    }
-    switch (proxyType?.toLowerCase()) {
-      case 'socks5':
-        agent = new SocksProxyAgent(
-          username
-            ? `socks://${username}:${password}@${host}:${port}`
-            : `socks://${host}:${port}`,
-        );
-        agentType = 'httpsAgent';
-        break;
-      case 'http':
-        agent = new HttpProxyAgent(
-          username
-            ? `http://${username}:${password}@${host}:${port}`
-            : `http://${host}:${port}`,
-        );
-        agentType = 'httpAgent';
-        break;
-      case 'https':
-        agent = new HttpsProxyAgent(
-          username
-            ? `http://${username}:${password}@${host}:${port}`
-            : `http://${host}:${port}`,
-        );
-        agentType = 'httpsAgent';
-        break;
+export const getProxy = (proxy) => {
+  if (!proxy) return;
+  const proxyConfig = proxy.split("://");
+  let protocol, host, port, username, password;
+  if (proxyConfig.length > 1) {
+    protocol = proxyConfig[0];
+    [host, port, username, password] = proxyConfig[1].split(":");
+  } else {
+    protocol = "http";
+    [host, port, username, password] = proxyConfig[0].split(":");
+  }
 
-      default:
-        break;
-    }
-
-    return { agent, agentType };
-}
+  return { protocol, host, port, username, password };
+};
